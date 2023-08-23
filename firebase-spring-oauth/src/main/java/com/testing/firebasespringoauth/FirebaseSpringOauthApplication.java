2ef9b383a32a26969e732115bf7ed8d7fceb2354
Deyvid Dimitrov <deyvid.dimitrov@qualifast.bg>
2023-08-23 11:05:24 +0300

    Initialize project

---
+++ firebase-spring-oauth/src/main/java/com/testing/firebasespringoauth/FirebaseSpringOauthApplication.java
@@ -0,0 +1,13 @@
+package com.testing.firebasespringoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseSpringOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirebaseSpringOauthApplication.class, args);
+	}
+
+}
---
+++ firebase-spring-oauth/src/test/java/com/testing/firebasespringoauth/FirebaseSpringOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.firebasespringoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseSpringOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
